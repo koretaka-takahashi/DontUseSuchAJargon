4c4f6ea76bcec6a33c1698f2f28ec1affb9a91f4
高橋是高 <shcrad.kore@gmail.com>
2020-08-27 18:49:49 +0900

    Add: Genreモデル作って一覧作成。（ジャンルでは今の所これだけで良いはず）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#index" # ←仮です
+  root "genres#index"
   resources :users, only: [:index, :show]
+  resources :genres, only: [:index]
   
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
