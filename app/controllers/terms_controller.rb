e2e318c2255638e83dafdf5bb14a308d11e1e3e4
高橋是高 <shcrad.kore@gmail.com>
2020-08-28 14:58:32 +0900

    Add: TermのCRUDを一通り作成。

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,4 +1,6 @@
 class TermsController < ApplicationController
+  before_action :set_term, only: [:show, :edit, :update, :destroy]
+
   def index
     @terms = Term.all
   end
@@ -23,12 +25,23 @@ class TermsController < ApplicationController
   end
 
   def update
+    if @term.update(term_params)
+      redirect_to terms_path, notice: "更新しました"
+    else
+      render :new
+    end
   end
 
   def destroy
+    @term.destroy
+    redirect_to terms_path, notice: "タイトル「#{@term.name}」を削除しました"
   end
 
   private
+
+  def set_term
+    @term = Term.find(params[:id])
+  end
 
   def term_params
     params.require(:term).permit(:name, :user_id, :genre_id)
--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -1,2 +1,9 @@
 module TermsHelper
+  def new_or_edit
+    if action_name == "new"
+      terms_path
+    elsif action_name == "edit"
+      term_path
+    end
+  end
 end
