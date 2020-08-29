4df109249c3d38bd5931546829758179beea11ba
高橋是高 <shcrad.kore@gmail.com>
2020-08-29 14:18:37 +0900

    Add: Usersテーブルにprofileカラム設置（必要と気づいたため）

---
+++ db/migrate/20200829045555_add_profile_to_users.rb
@@ -0,0 +1,5 @@
+class AddProfileToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :profile, :text, default: "", null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_032041) do
+ActiveRecord::Schema.define(version: 2020_08_29_045555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_08_28_032041) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile", default: "", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
