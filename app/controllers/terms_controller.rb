a2ffce9832f64fae4bf7f0e412e663514dbf8784
高橋是高 <shcrad.kore@gmail.com>
2020-09-02 16:11:26 +0900

    Changed: Termの編集、削除の権限を作成者にも与えないように変更

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,8 +1,9 @@
 class TermsController < ApplicationController
   before_action :set_term, only: [:show, :edit, :update, :destroy]
-  before_action :set_genres, only: [:new, :edit]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :user_check, only: [:edit, :update, :destroy]
+  before_action :set_genres, only: [:new, :edit] # 選択ボックスの為ジャンルを全てセット。
+  before_action :authenticate_user!, except: [:index, :show] # ログイン済みかどうか。
+  # before_action :user_check, only: [:edit, :update, :destroy]
+  # ↑ 作成者かどうか。現在は不要と判断。いずれ管理者機能実装時に管理者にのみ権限付与予定。
 
   def index
     @terms = Term.all
