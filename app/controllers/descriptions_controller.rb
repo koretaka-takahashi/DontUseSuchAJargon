901a988f8b953e67965e85bfd9f61ce30b76ce1e
高橋是高 <shcrad.kore@gmail.com>
2020-09-02 14:29:16 +0900

    Add: 解説の編集、削除機能を追加

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,5 +1,5 @@
 class DescriptionsController < ApplicationController
-  before_action :set_term, only: [:show, :edit, :update, :destroy]
+  before_action :set_description, only: [:show, :edit, :update, :destroy]
   # before_action :set_genres, only: [:new, :edit]
   # before_action :authenticate_user!, except: [:index, :show]
   # before_action :user_check, only: [:edit, :update, :destroy]
--- app/helpers/descriptions_helper.rb
+++ app/helpers/descriptions_helper.rb
@@ -1,7 +1,7 @@
 module DescriptionsHelper
   def description_new_or_edit
     if action_name == "new"
-      term_descriptions_path(@description.term)
+      term_descriptions_path
     elsif action_name == "edit"
       term_description_path
     end
