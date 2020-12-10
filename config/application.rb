1d4cff37d44c440eeae5a0b3c42dfc9185578ddf
高橋是高 <shcrad.kore@gmail.com>
2020-12-10 11:59:34 +0900

    Add: タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module DontUseSuchAJargon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
 
     #【NameError: uninitialized constant User::ImageUploaderの対策】Carrierwaveでのエラーの模様。
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
