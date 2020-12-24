ecfde10d85a888ca237eb7768192ef61c4cdc0ee
高橋是高 <shcrad.kore@gmail.com>
2020-12-24 12:22:36 +0900

    Fix: タグのモデルスペックが落ちるのを修正（最大文字数を変えたのを忘れていた為）

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe Tag, type: :model do
     tag.valid?
     expect(tag.errors[:name]).to include("を入力してください")
   end
-  it "is invalid if the name is greater than 20 characters" do
-    tag = FactoryBot.build(:tag1, name: "a" * 21, user_id: @user.id, genre_id: @genre.id)
+  it "is invalid if the name is greater than 50 characters" do
+    tag = FactoryBot.build(:tag1, name: "a" * 51, user_id: @user.id, genre_id: @genre.id)
     tag.valid?
-    expect(tag.errors[:name]).to include("は20文字以内で入力してください")
+    expect(tag.errors[:name]).to include("は50文字以内で入力してください")
   end
   it "is valid if the name is present and within 20 characters" do
     tag = FactoryBot.build(:tag1, name: "a" * 20, user_id: @user.id, genre_id: @genre.id)
